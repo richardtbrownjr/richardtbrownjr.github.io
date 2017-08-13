2fc96a0bcadf2c25e21bc37f075ad9480a869e64
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2017-08-13 16:58:33 -0400

    fitbit verify try 2

--- js/fitbicall.js
+++ js/fitbicall.js
@@ -1,6 +1,6 @@
 function UserAction() {
     var xhttp = new XMLHttpRequest();
-    GET https://yourapp.com/fitbit/webhook?verify=9c94a5f3891cac9874da149bb5a0d40e200995fb452e8b5a0c8291bb36ac7e94
+    GET https://https://richardtbrownjr.github.io/webhook?verify=9c94a5f3891cac9874da149bb5a0d40e200995fb452e8b5a0c8291bb36ac7e94
     xhttp.open("POST", "Your Rest URL Here", false);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();

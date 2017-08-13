6123dd55b2aa5e5e9429d4d17fe988e85cf9bb47
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2017-08-13 19:46:41 -0400

    fitbit verify sub 5

--- js/fitbicall.js
+++ js/fitbicall.js
@@ -1,4 +1,4 @@
 function UserAction() {
-    var xhttp = new XMLHttpRequest();
+    
     GET https://https://richardtbrownjr.github.io/webhook?verify=9c94a5f3891cac9874da149bb5a0d40e200995fb452e8b5a0c8291bb36ac7e94
     }
